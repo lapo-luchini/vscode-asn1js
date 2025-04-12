253799a73ae2f10cb4f5d1d726d13180ca24c1fa
Lapo Luchini <lapo@lapo.it>
2025-04-12 14:32:33 +0000

    npx @eslint/migrate-config .eslintrc.json

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import { defineConfig } from "eslint/config";
+import { includeIgnoreFile } from "@eslint/compat";
+import globals from "globals";
+import { fileURLToPath } from "node:url";
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
+export default defineConfig([
+    includeIgnoreFile(gitignorePath),
+    {
+        languageOptions: {
+            globals: {
+                ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, "off"])),
+                ...globals.commonjs,
+                ...globals.node,
+                ...globals.mocha,
+            },
+
+            ecmaVersion: 2018,
+            sourceType: "module",
+
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+
+        rules: {
+            "no-const-assign": "warn",
+            "no-this-before-super": "warn",
+            "no-undef": "warn",
+            "no-unreachable": "warn",
+            "no-unused-vars": "warn",
+            "constructor-super": "warn",
+            "valid-typeof": "warn",
+        },
+    }
+]);
